3b1e3b1a7fb52d0c8e8e0844bc64d24414fa206b
Andrew <andrewfoster8426@gmail.com>
2022-02-01 20:15:27 -0500

    Add scanner

--- src/uml/Driver.java
+++ src/uml/Driver.java
@@ -4,9 +4,30 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args){
-        String message = "Hello world!";
-        String message2 = "Hello world!";
-        System.out.println(message);
 
+        String prompt = "> ";
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(prompt);
+        while(true){
+            System.out.println("before scan.nextln");
+            String input = scan.nextLine();
+            System.out.println("after scan.nextln");
+
+            switch (input){
+                case "add class":
+                    System.out.println("Enter class name: ");
+                    input = scan.next();
+                    //System.out.println("You added class " + input);
+                case "":
+                    System.out.print(prompt);
+                case "exit":
+                    return;
+                default:
+                    System.out.println("Please enter a valid command: ");
+            }
+        }
+        
     }
 }
