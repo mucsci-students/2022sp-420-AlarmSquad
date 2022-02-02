7aa0019cf6d66acfd2ca4e250e16f4bce4bb9775
lwsantee <lwsantee@protonmail.com>
2022-02-02 13:29:54 -0500

    added class, attributes, and relationship functionality

--- src/uml/Class.java
+++ src/uml/Class.java
@@ -2,50 +2,82 @@ package uml;
 
 import java.util.ArrayList;
 
-import javax.print.attribute.Attribute;
-
+/**
+ * A class for the UML class we will be working with in our diagram.
+ * 
+ * @authors
+ */
 public class Class {
-    String name;
-    ArrayList<Attribute> attributes;
-    ArrayList<Relationship> incomingRelationships;
-    ArrayList<Relationship> outgoingRelationships;
 
+    // The name of the class
+    private String name;
+    // The arraylist of attributes in the class
+    private ArrayList<Attribute> attributes;
+    // The arraylist of relationships the class has
+    private ArrayList<Relationship> relationships;
+
+    /**
+     * Default constructor
+     * 
+     * @param name the name of the class
+     */
     public Class(String name) {
         this.name = name;
-        // this.attributes = new ArrayList<Attribute>();
-        // this.incomingRelationships = new ArrayList<Relationship>();
-        // this.outgoingRelationships = new ArrayList<Relationship>();
+        this.attributes = new ArrayList<Attribute>();
+        this.relationships = new ArrayList<Relationship>();
     }
 
-    public void setName() {
-
+    /**
+     * Sets the name of the class
+     * 
+     * @param name the new name for the class
+     */
+    public void setName(String name) {
+        this.name = name;
     }
 
+    /**
+     * Gets the name of the class
+     * 
+     * @return the name of the class
+     */
     public String getName() {
-        return "";
+        return this.name;
     }
 
+    /**
+     * Adds an attribute to the end of the attribute list
+     * 
+     * @param attribute the attribute to be added
+     */
     public void addAttribute(Attribute attribute) {
         this.attributes.add(attribute);
     }
 
+    /**
+     * Deletes the first occurance of the attribute from the attribute list
+     * 
+     * @param attribute the attribute to be deleted
+     */
     public void deleteAttribute(Attribute attribute) {
         this.attributes.remove(attribute);
     }
 
+    /**
+     * Adds a relationship to the end of the relationship list
+     * 
+     * @param relationship the relationship to be added
+     */
     public void addIncomingRelationship(Relationship relationship) {
-        this.incomingRelationships.add(relationship);
+        this.relationships.add(relationship);
     }
 
+    /**
+     * Deletes the first occurance of the relationship from the relationship list
+     * 
+     * @param relationship the relationship to be deleted
+     */
     public void deleteIncomingRelationship(Relationship relationship) {
-        this.incomingRelationships.remove(relationship);
-    }
-
-    public void addOutgoingRelationship(Relationship relationship) {
-        this.outgoingRelationships.add(relationship);
-    }
-
-    public void deleteOutgoingRelationship(Relationship relationship) {
-        this.outgoingRelationships.remove(relationship);
+        this.relationships.remove(relationship);
     }
 }
--- src/uml/Driver.java
+++ src/uml/Driver.java
@@ -3,36 +3,75 @@ package uml;
 import java.util.*;
 
 /**
- * Get user input, then use switch statement to process commands.
- * Issue: when user just presses enter as input.
+ * Gets user input, then uses switch statements to process commands.
+ * 
+ * @authors
  */
 public class Driver {
 
-    public static void main(String[] args){
+    // Default prompt for user
+    private static String prompt = "> ";
+    // Creates new scanner
+    private static Scanner scan = new Scanner(System.in);
+    // The arraylist of classes in the diagram
+    private static ArrayList<Class> classes;
 
-        String prompt = "> ";
-        
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
 
         System.out.print(prompt);
-        while(true){
-            
+
+        // Enters the command loop
+        while (true) {
+
+            // Takes in the next line of user input
             String input = scan.nextLine();
 
-
-            if (input.equals(null)){
+            // If the user enters a blank line, prompt again
+            if (input.equals("")) {
                 System.out.print(prompt);
-            }
-            else{
-                switch (input){
+            } else {
+                // If the user enters a command
+                switch (input) {
                     case "add class":
-                        System.out.println("Enter class name: ");
-                        input = scan.next();
-                        //System.out.println("You added class " + input);
+                        System.out.print("Enter class name: ");
+                        String className = scan.next();
+                        System.out.println("You added class \"" + className + "\"");
+                        Class newClass = new Class(className);
+                        break;
+                    case "delete class":
+                        break;
+                    case "rename class":
+                        break;
+                    case "add attribute":
+                        break;
+                    case "delete attribute":
+                        break;
+                    case "rename attribute":
+                        break;
+                    case "add relationship":
+                        break;
+                    case "delete relationship":
+                        break;
+                    case "rename relationship":
+                        break;
+                    case "save":
+                        break;
+                    case "load":
+                        break;
+                    case "list classes":
+                        break;
+                    case "list class":
+                        break;
+                    case "list relationships":
+                        break;
+                    case "help":
+                        break;
                     case "exit":
                         return;
+                    // If the command is not valid
                     default:
-                        System.out.println("Please enter a valid command: ");
+                        System.out.println("Please enter a valid command");
+                        System.out.print(prompt);
                 }
             }
         }
