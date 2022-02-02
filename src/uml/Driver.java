52fbc05407b39bbd2307de8260b16a76f2395ea2
Andrew <andrewfoster8426@gmail.com>
2022-02-02 14:37:04 -0500

    Start adding add attribute. Broken tho.

--- src/uml/Driver.java
+++ src/uml/Driver.java
@@ -2,6 +2,8 @@ package uml;
 
 import java.util.*;
 
+import javax.management.Attribute;
+
 /**
  * Gets user input, then uses switch statements to process commands.
  * 
@@ -14,7 +16,7 @@ public class Driver {
     // Creates new scanner
     private static Scanner scan = new Scanner(System.in);
     // The arraylist of classes in the diagram
-    private static ArrayList<Class> classes;
+    private static ArrayList<Class> classes = new ArrayList<Class>();
 
     public static void main(String[] args) {
 
@@ -32,17 +34,39 @@ public class Driver {
             } else {
                 // If the user enters a command
                 switch (input) {
+                    case "exit":
+                        return;
                     case "add class":
+                        // Get user defined name for class, then add to ArrayList classes
                         System.out.print("Enter class name: ");
                         String className = scan.next();
                         System.out.println("You added class \"" + className + "\"");
                         Class newClass = new Class(className);
+                        classes.add(newClass);
                         break;
                     case "delete class":
                         break;
                     case "rename class":
                         break;
                     case "add attribute":
+                        // Finds correct class to add attribute(Att)
+
+                        System.out.print("First enter class name: ");
+                        String classToAddAtt = scan.next();
+
+                        for (int i = 0; i < classes.size(); ++i) {
+                            if (classToAddAtt == classes.get(i).getName()) {
+                                // Adds the Attribure to the desired class
+                                System.out.print("Enter attribute name: ");
+                                String attributeName = scan.next();
+                                Attribute attribute = new Attribute(attributeName);
+                                classes.get(i).addAttribute(attribute);
+                                return;
+                            } else
+                                System.out
+                                        .println(classToAddAtt + " class was not found, please try again or add class");
+                        }
+
                         break;
                     case "delete attribute":
                         break;
@@ -59,6 +83,10 @@ public class Driver {
                     case "load":
                         break;
                     case "list classes":
+                        // outer loop iterating through classes
+                        for (int i = 0; i < classes.size(); ++i) {
+                            System.out.println(classes.get(i).getName());
+                        }
                         break;
                     case "list class":
                         break;
@@ -66,8 +94,6 @@ public class Driver {
                         break;
                     case "help":
                         break;
-                    case "exit":
-                        return;
                     // If the command is not valid
                     default:
                         System.out.println("Please enter a valid command");
