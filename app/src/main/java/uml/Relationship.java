e740c7be8a9c89ee44147d05ac1e48779bc43491
Andrew <andrewfoster8426@gmail.com>
2022-02-11 20:42:31 -0500

    impliment gradle stuff

---
+++ app/src/main/java/uml/Relationship.java
@@ -0,0 +1,86 @@
+package uml;
+
+/**
+ * A class for the relationship object between two UML Class objects
+ * 
+ * @authors
+ */
+public class Relationship {
+
+    // The id of the relationship
+    private String id;
+    // The source of the relationship
+    private Class source;
+    // The destination of the relationship
+    private Class destination;
+
+
+    /**
+     * Default constructor
+     * 
+     * @param source      the source of the relationship
+     * @param destination the destination of the relationship
+     */
+
+    public Relationship(Class source, Class destination) {
+        this.id = source.getClassName() + destination.getClassName();
+        this.source = source;
+        this.destination = destination;
+    }
+
+    /**
+     * Sets the id of the relationship
+     * 
+     * @param id the new id of the relationship
+     */
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the id of the relationship
+     * 
+     * @return the id of the relationship
+     */
+    public String getID() {
+        return this.id;
+    }
+
+    /**
+     * Sets the source of the relationship
+     * 
+     * @param source the new source of the relationship
+     */
+    public void setSource(Class source) {
+        this.source = source;
+    }
+
+    /**
+     * Gets the source of the relationship
+     * 
+     * @return the source of the relationship
+     */
+    public Class getSource() {
+        return this.source;
+    }
+
+    /**
+     * Sets the destination of the relationship
+     * 
+     * @param destination the new destination of the relationship
+     */
+    public void setDestination(Class destination) {
+        this.destination = destination;
+    }
+
+    /**
+     * Gets the destination of the relationship
+     * 
+     * @return the destination of the relationship
+     */
+    public Class getDestination() {
+        return this.destination;
+    }
+
+    
+}
---
+++ app/src/test/java/uml/RelationshipTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import uml.*;
+import org.junit.Test;
+
+/**
+ * JUnit tests for Relationship
+ * 
+ * @authors
+ */
+public class RelationshipTest {
+
+    // @Test
+    // public boolean test01Constructor() {
+    //     return true;
+    // }
+}
