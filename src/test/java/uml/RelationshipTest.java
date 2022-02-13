1448aff5a190aeed49a03974d84c56a29392586c
anOrgandroiD <anorgandroid@outlook.com>
2022-02-13 18:14:52 -0500

    updated tests to run altogether

--- src/test/java/uml/RelationshipTest.java
+++ src/test/java/uml/RelationshipTest.java
@@ -2,11 +2,10 @@ package uml;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Scanner;
-
 import org.junit.Test;
+
+import uml.managers.ClassManager;
+import uml.managers.RelationshipManager;
 
 /**
  * JUnit tests for Relationship
@@ -14,80 +13,39 @@ import org.junit.Test;
  * @authors
  */
 public class RelationshipTest {
+    private static ClassManager classManager = new ClassManager();
+    private static RelationshipManager relationshipManager = new RelationshipManager();
 
     @Test
     public void testAddRel() {
-        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\nexit\nStudent\nTeacher";
-        InputStream stdin = System.in;
-
-        try {
-            System.setIn(new ByteArrayInputStream(data.getBytes()));
-            Scanner scan = new Scanner(System.in);
-            
-            Driver.runCLI();
-
-            assertEquals("StudentTeacher", Driver.findRelationship().getID());
-
-            scan.close();
-        } finally {
-            System.setIn(stdin);
-        }
+        classManager.addClass("Student");
+        classManager.addClass("Teacher");
+        relationshipManager.addRelationship(classManager, "Student", "Teacher");
+        assertEquals(true, relationshipManager.findRelationship(classManager, "Student", "Teacher") != null);
+        assertEquals(false, relationshipManager.findRelationship(classManager, "Student", "Student") != null);
+        assertEquals(false, relationshipManager.findRelationship(classManager, "Student", "Professor") != null);
     }
 
     @Test
     public void testAddDuplicateRel() {
-        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\na rel\nStudent\nTeacher\nexit\nStudent\nTeacher";
-        InputStream stdin = System.in;
-
-        try {
-            System.setIn(new ByteArrayInputStream(data.getBytes()));
-            Scanner scan = new Scanner(System.in);
-            
-            Driver.runCLI();
-
-            assertEquals("StudentTeacher", Driver.findRelationship().getID());
-
-            scan.close();
-        } finally {
-            System.setIn(stdin);
-        }
+        classManager.addClass("Student");
+        classManager.addClass("Teacher");
+        relationshipManager.addRelationship(classManager, "Student", "Teacher");
+        assertEquals("Relationship already exists between \"Student\" and \"Teacher\"\n", relationshipManager.addRelationship(classManager, "Student", "Teacher"));
     }
 
     @Test
     public void testDelRel() {
-        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\nd rel\nStudent\nTeacher\ny\nexit\nStudent\nTeacher";
-        InputStream stdin = System.in;
-
-        try {
-            System.setIn(new ByteArrayInputStream(data.getBytes()));
-            Scanner scan = new Scanner(System.in);
-            
-            Driver.runCLI();
-
-            assertEquals(null, Driver.findRelationship());
-
-            scan.close();
-        } finally {
-            System.setIn(stdin);
-        }
+        classManager.addClass("Student");
+        classManager.addClass("Teacher");
+        relationshipManager.addRelationship(classManager, "Student", "Teacher");
+        relationshipManager.deleteRelationship(classManager, "Student", "Teacher");
+        assertEquals(false, relationshipManager.findRelationship(classManager, "Student", "Teacher") != null);
     }
 
     @Test
     public void testDelNonExistantRel() {
-        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\nd rel\nTeacher\nStudent\nexit\nStudent\nTeacher";
-        InputStream stdin = System.in;
-
-        try {
-            System.setIn(new ByteArrayInputStream(data.getBytes()));
-            Scanner scan = new Scanner(System.in);
-            
-            Driver.runCLI();
-
-            assertEquals("StudentTeacher", Driver.findRelationship().getID());
-
-            scan.close();
-        } finally {
-            System.setIn(stdin);
-        }
+        classManager.addClass("Student");
+        assertEquals("Relationship not found between \"Student\" and \"Student\"\n", relationshipManager.deleteRelationship(classManager, "Student", "Student"));
     }
 }
