9c9391d02e9ad7afbd54b6a14c2cf60d8213bf08
anOrgandroiD <anorgandroid@outlook.com>
2022-02-13 12:12:24 -0500

    created tests for relationships

--- src/test/java/uml/RelationshipTest.java
+++ src/test/java/uml/RelationshipTest.java
@@ -1,6 +1,12 @@
 package uml;
 
-// import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.junit.Test;
 
 /**
  * JUnit tests for Relationship
@@ -9,8 +15,79 @@ package uml;
  */
 public class RelationshipTest {
 
-    // @Test
-    // public boolean test01Constructor() {
-    //     return true;
-    // }
+    @Test
+    public void testAddRel() {
+        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\nexit\nStudent\nTeacher";
+        InputStream stdin = System.in;
+
+        try {
+            System.setIn(new ByteArrayInputStream(data.getBytes()));
+            Scanner scan = new Scanner(System.in);
+            
+            Driver.runCLI();
+
+            assertEquals("StudentTeacher", Driver.findRelationship().getID());
+
+            scan.close();
+        } finally {
+            System.setIn(stdin);
+        }
+    }
+
+    @Test
+    public void testAddDuplicateRel() {
+        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\na rel\nStudent\nTeacher\nexit\nStudent\nTeacher";
+        InputStream stdin = System.in;
+
+        try {
+            System.setIn(new ByteArrayInputStream(data.getBytes()));
+            Scanner scan = new Scanner(System.in);
+            
+            Driver.runCLI();
+
+            assertEquals("StudentTeacher", Driver.findRelationship().getID());
+
+            scan.close();
+        } finally {
+            System.setIn(stdin);
+        }
+    }
+
+    @Test
+    public void testDelRel() {
+        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\nd rel\nStudent\nTeacher\ny\nexit\nStudent\nTeacher";
+        InputStream stdin = System.in;
+
+        try {
+            System.setIn(new ByteArrayInputStream(data.getBytes()));
+            Scanner scan = new Scanner(System.in);
+            
+            Driver.runCLI();
+
+            assertEquals(null, Driver.findRelationship());
+
+            scan.close();
+        } finally {
+            System.setIn(stdin);
+        }
+    }
+
+    @Test
+    public void testDelNonExistantRel() {
+        String data = "a class\nStudent\na class\nTeacher\na rel\nStudent\nTeacher\nd rel\nTeacher\nStudent\nexit\nStudent\nTeacher";
+        InputStream stdin = System.in;
+
+        try {
+            System.setIn(new ByteArrayInputStream(data.getBytes()));
+            Scanner scan = new Scanner(System.in);
+            
+            Driver.runCLI();
+
+            assertEquals("StudentTeacher", Driver.findRelationship().getID());
+
+            scan.close();
+        } finally {
+            System.setIn(stdin);
+        }
+    }
 }
