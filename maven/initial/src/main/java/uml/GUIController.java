dfcf309338fcbac1dd2f69067ae4cf607703cf78
lwsantee <lwsantee@protonmail.com>
2022-02-25 14:16:19 -0500

    start gui boilerplate code

--- maven/initial/src/main/java/uml/GUIController.java
+++ maven/initial/src/main/java/uml/GUIController.java
@@ -3,7 +3,7 @@ package uml;
 public class GUIController {
 
     public static void main(String[] args) {
-        
+        GUIView.main(args);
     }
 
 }
--- maven/initial/src/main/java/uml/GUIView.java
+++ maven/initial/src/main/java/uml/GUIView.java
@@ -1,4 +1,27 @@
 package uml;
 
-public class GUIView {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class GUIView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene window = new Scene(root, Color.BLACK);
+
+        stage.setTitle("UML Editor");
+        stage.setWidth(420);
+        stage.setHeight(420);
+
+        stage.setScene(window);
+        stage.show();
+    }
 }
