2c6b9884b6a55f4f2cc9f500672583219d8b3501
Logan <lwsantee@protonmail.com>
2022-03-22 17:52:05 -0400

    add location saving for classBoxes in the GUI

--- maven/initial/src/main/java/uml/ClassBox.java
+++ maven/initial/src/main/java/uml/ClassBox.java
@@ -21,6 +21,8 @@ public class ClassBox {
     private Text classTitle;
     private ArrayList<Text> fieldTextList;
     private ArrayList<Text> methTextList;
+    private double x;
+    private double y;
 
     public ClassBox(String className) {
         classBoxName = className;
@@ -37,8 +39,15 @@ public class ClassBox {
         classPane.getChildren().add(1, flow);
     }
 
-
     // Getters and Setters
+    public double getX() {return x;}
+
+    public void setX(double x) {this.x = x;}
+
+    public double getY() {return y;}
+
+    public void setY(double y) {this.y = y;}
+
     public StackPane getClassPane() {
         return this.classPane;
     }
