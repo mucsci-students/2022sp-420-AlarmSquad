f71ab29df447aca46c9e5127ef459cf146197ca9
mmoyer1 <70113537+mmoyer1@users.noreply.github.com>
2022-04-24 19:07:16 -0400

    Update MementoTest.java

--- maven/initial/src/test/java/uml/MementoTest.java
+++ maven/initial/src/test/java/uml/MementoTest.java
@@ -6,7 +6,17 @@ public class MementoTest extends TestCase {
 
     UMLModel model = new UMLModel();
     UMLClass student = new UMLClass("student");
+    Caretaker caretaker = new Caretaker();
 
+    public void testMemento(){
+        Memento memento = new Memento(model);
+        caretaker.pushToUndoStack(memento);
+        memento.getState().addClass(student);
+        assertEquals("student", memento.getState().getClassList().get(0).getClassName());
+        caretaker.undoHelper(memento);
+        assertEquals(true, caretaker.undoStackIsEmpty());
+    }
+    
     public void testGetState() {
         model.addClass(student);
         Memento undo = new Memento(model);
